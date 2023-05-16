c0afa31f8de9848924e796da34d68fa04a95a9a3
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2023-05-16 18:21:14 -0400

    att_Troca e Adicaode Perguntas ao Fim do tempo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Perguntas")]
     [SerializeField] private PerguntasSO perguntaAtual;
+    [SerializeField] private PerguntasSO[] perguntasTotais;
     [SerializeField] private TextMeshProUGUI enunciadoTexto;
     [SerializeField] private GameObject[] alternativaTMP;
-    [Header ("sprites")]
+    [Header("sprites")]
     [SerializeField] private Sprite SpriteCorreta;
     [SerializeField] private Sprite SpriteIncorreta;
-
+    [SerializeField] private Sprite spritePadrao;
+    [SerializeField] private GameObject inicioDoJogoCanvas;
+    [SerializeField] private GameObject quizJogoCanvas;
+    [SerializeField] private GameObject fimDeJogoCanvas;
+    private Temporizador temporizador;
+    private int posicaoVetor;
+    private int contagemRespostasCorreta;
+    [SerializeField] private TextMeshProUGUI mensagemFimdeJogo;
     void Start()
     {
+        posicaoVetor = 0;
+        contagemRespostasCorreta = 0;
+        temporizador = GetComponent<Temporizador>();
+        temporizador.RegistrarParada(OnParadaTimer);
         enunciadoTexto.SetText(perguntaAtual.GetEnunciado());
         string[] alternativas = perguntaAtual.GetAlternativas();
-        for(int i = 0; i < alternativas.Length; i++)
+        for (int i = 0; i < alternativas.Length; i++)
         {
             TextMeshProUGUI alternativa = alternativaTMP[i].GetComponentInChildren<TextMeshProUGUI>();
             alternativa.SetText(alternativas[i]);
@@ -25,17 +39,17 @@ public class GameManager : MonoBehaviour
     }
     public void HandleOption(int alternativaSelecionada)
     {
+        DisableOptionButtons();
+        PararTimer();
         Image imageAlt = alternativaTMP[alternativaSelecionada].GetComponent<Image>();
-        
         if (alternativaSelecionada == perguntaAtual.GetAlternativaCorreta())
         {
             ChangeButtonSprite(imageAlt, SpriteCorreta);
-            DisableOptionButtons();
+            contagemRespostasCorreta++;
         }
         else
         {
             ChangeButtonSprite(imageAlt, SpriteIncorreta);
-            DisableOptionButtons();
             ChangeButtonSprite(alternativaTMP[perguntaAtual.GetAlternativaCorreta()].GetComponent<Image>(), SpriteCorreta);
         }
     }
@@ -45,11 +59,77 @@ public class GameManager : MonoBehaviour
     }
     public void DisableOptionButtons()
     {
-        for(int i = 0; i< alternativaTMP.Length; i++)
+        for (int i = 0; i < alternativaTMP.Length; i++)
         {
             Button btnAlt = alternativaTMP[i].GetComponent<Button>();
             btnAlt.enabled = false;
         }
     }
-    
+    void ReiniciarOptionsButtons()
+    {
+        for (int i = 0; i < alternativaTMP.Length; i++)
+        {
+            Button btnAlt = alternativaTMP[i].GetComponent<Button>();
+            btnAlt.enabled = true;
+        }
+        for (int i = 0; i < alternativaTMP.Length; i++)
+        {
+            Image imageBtn = alternativaTMP[i].GetComponent<Image>();
+            ChangeButtonSprite(imageBtn, spritePadrao);
+        }
+        temporizador.Zerar();
+    }
+    public void PopularPergunta()
+    {
+        perguntaAtual = perguntasTotais[posicaoVetor];
+        enunciadoTexto.SetText(perguntaAtual.GetEnunciado());
+        string[] alternativas = perguntaAtual.GetAlternativas();
+        for (int i = 0; i < alternativas.Length; i++)
+        {
+            TextMeshProUGUI alternativa = alternativaTMP[i].GetComponentInChildren<TextMeshProUGUI>();
+            alternativa.SetText(alternativas[i]);
+        }
+        
+    }
+    void PararTimer()
+    {
+        temporizador.Parar();
+    }
+    private void OnParadaTimer()
+    { 
+        if (posicaoVetor >= perguntasTotais.Length - 1){
+
+            Debug.Log("Fimde Jogo");
+            Invoke("HabilitarTelaFimdeJogo", 1f);
+        }
+        else{
+                Invoke("AlterarPergunta",1f);
+            }
+    }
+    void AlterarPergunta()
+    {
+        posicaoVetor++;
+        //posicaoVetor = 0;
+        //contagemRespostasCorreta = 0;
+        PopularPergunta();
+        ReiniciarOptionsButtons();
+    }
+    public void JogarNovamente()
+    {
+        posicaoVetor = 0;
+        contagemRespostasCorreta = 0;
+        fimDeJogoCanvas.SetActive(false);
+        quizJogoCanvas.SetActive(true);
+        PopularPergunta();
+        ReiniciarOptionsButtons();
+    }
+    void HabilitarTelaFimdeJogo()
+    {
+        mensagemFimdeJogo.SetText("Parabéns Você Acertou "+ contagemRespostasCorreta+" de "+ perguntasTotais.Length);
+        fimDeJogoCanvas.SetActive(true);
+        quizJogoCanvas.SetActive(false);
+    }
+
+
+
 }
--- Assets/Scripts/Temporizador.cs
+++ Assets/Scripts/Temporizador.cs
@@ -4,27 +4,58 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Temporizador : MonoBehaviour
 {
-    [SerializeField] float tempoMaximo;
+    // Delegate
+    // Delegates são um tipo de dados que representam referências a um ou mais métodos 
+    public delegate void OnParadaTimer();
+    public OnParadaTimer onParadaTimer;
+
+    [SerializeField] private float tempoMaximo;
+    [SerializeField] private Slider slider;
     private float tempoAtual;
-    private Slider sliderCp;
-    // Start is called before the first frame update
+    private bool estaContando;
+
+
     void Start()
     {
         tempoAtual = 0f;
-        sliderCp = GetComponent<Slider>();
-        sliderCp.maxValue = tempoMaximo;
-        sliderCp.value = tempoAtual;
+        slider.maxValue = tempoMaximo;
+        slider.value = tempoAtual;
+        estaContando = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        tempoAtual += 1 * Time.deltaTime;
-        if (tempoAtual > tempoMaximo)
+        if (estaContando)
         {
-            Debug.Log("acabou o tempo");
-            tempoAtual = 0f;
+            tempoAtual += 1 * Time.deltaTime;
+            slider.value = tempoAtual;
+
+            if (tempoAtual > tempoMaximo && onParadaTimer != null)
+            {
+                Parar();
+            }
         }
-        sliderCp.value = tempoAtual;
+    }
+
+    // Registro do método para o delegate
+    public void RegistrarParada(OnParadaTimer metodo)
+    {
+        onParadaTimer += metodo;
+    }
+
+    public void Parar()
+    {
+        estaContando = false;
+
+        if (onParadaTimer != null)
+        {
+            onParadaTimer();
+        }
+    }
+
+    public void Zerar()
+    {
+        tempoAtual = 0f;
+        slider.value = tempoAtual;
+        estaContando = true;
     }
 }
